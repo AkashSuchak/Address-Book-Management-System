b762577d037857fe04085ee66cbe9adb4d6b7a2f
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-09 15:45:35 +0530

    feat: Welcome Message

---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display Welcome Message
+            Console.WriteLine("Welcome to Address Book Management System Development!");
+        }
+    }
+}
