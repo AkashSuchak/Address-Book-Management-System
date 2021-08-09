543fa99025e783b89068d73d9bcaca06c760ad5c
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-09 18:17:14 +0530

    feat: Added : Contact added in AddressBook

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,12 +6,47 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+
             //Display Welcome Message
             Console.WriteLine("Welcome to Address Book Management System Development!");
             Console.WriteLine("======================================================");
 
-            //UserInput Class Object
-            UserInput userInput = new UserInput();
+            //Variables
+            string firstName, lastName, address, city, state, email, phoneNumber;
+            int zipCode;
+
+            Console.WriteLine("Fill the Contact Details to add in Address Book:");
+            Console.WriteLine("------------------------------------------------");
+
+            //User Inputs
+            Console.WriteLine("Enter First Name : "); 
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name : ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address : ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter City Name : ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter State Name : ");
+            state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip-Code/ Pin-Code : ");
+            zipCode = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number : ");
+            phoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email-ID : ");
+            email = Console.ReadLine();                        
+            
+            //Add Contact
+            AddressBookBuilder addressBookBuilder = new AddressBookBuilder();
+            addressBookBuilder.AddContact(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+            
         }
     }
 }
