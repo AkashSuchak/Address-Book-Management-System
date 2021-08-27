d9208fd7e4eefc31c03d1493a805cfbc88f80e84
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-27 18:06:55 +0530

    feat: Added: Multiple AddressBook With multiple Contact Details

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,17 +9,13 @@ namespace AddressBook
             //Display Welcome Message
             Console.WriteLine("Welcome to Address Book Management System Development!");
             Console.WriteLine("======================================================");
-
-            //Object of Contact Details
-            ContactDetails CD = new ContactDetails();
-
-            //Add Contact 
+            
+            //Call AddressBook Options
             AddressBookBuilder addressBookBuilder = new AddressBookBuilder();
-            addressBookBuilder.AddContact(CD.firstName, CD.lastName, CD.address, CD.city, CD.state, CD.zipCode, CD.phoneNumber, CD.email);
-            addressBookBuilder.Display(); //Display Contact
+            addressBookBuilder.AddressBookCreator();
 
-           //addressBookBuilder.ModifyContact();//Modify Contact
-
+            //addressBookBuilder.Display(); //Display Contact
+            //addressBookBuilder.ModifyContact();//Modify Contact
             //addressBookBuilder.DeleteContact();//Delete Contact
         }
     }
